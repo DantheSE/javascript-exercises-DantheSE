c54ecca0ee9e63cc36b77dbdeb1d6f6ce5c4a80b
DantheSE <dan.doyin@gmail.com>
2024-05-19 03:40:13 +0100

    completed challenge 3, all tests passed successfully

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+let userInputString;
 
+const reverseString = function (userInputString) {
+  let returnedString = "";
+  for (i = userInputString.length - 1; i >= 0; i--) {
+    returnedString += userInputString[i];
+  } 
+  return returnedString;
 };
 
 // Do not edit below this line
