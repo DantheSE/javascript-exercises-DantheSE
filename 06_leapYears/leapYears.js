29f8ba3c0de1c619ffd4e7aa36120e4205d197a8
DantheSE <dan.doyin@gmail.com>
2024-05-19 22:54:50 +0100

    completed challenge 6, all tests checked successfully

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,19 @@
-const leapYears = function() {
+let storedYear;
 
+const leapYears = function (storedYear) {
+  if (storedYear % 4 === 0) {
+    if (storedYear % 400 === 0 || storedYear % 4 === 0) {
+      if ((storedYear % 400 === 0 && storedYear % 4 === 0) || (storedYear % 100 !== 0 && storedYear % 4 === 0)) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
