afbabd65f3848175b22a320449c4691b25c6511f
DantheSE <dan.doyin@gmail.com>
2024-05-19 12:43:25 +0100

    completed challenge 5, all tests passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,41 @@
-const sumAll = function() {
+let rangeStart;
+let rangeEnd;
 
+const sumAll = function (rangeStart, rangeEnd) {
+  let sum = 0;
+
+  if (
+    rangeStart < rangeEnd &&
+    !(rangeStart < 0 || rangeEnd < 0) &&
+    !(typeof rangeStart === "string" || typeof rangeEnd === "string")
+  ) {
+    for (let i = rangeStart; i <= rangeEnd; i++) {
+      if (i <= rangeEnd) {
+        sum += i;
+      }
+    }
+  } else if (
+    rangeStart > rangeEnd &&
+    !(typeof rangeStart === "string" || typeof rangeEnd === "string")
+  ) {
+    for (let i = rangeStart; i >= rangeEnd; i--) {
+      if (i >= rangeEnd) {
+        sum += i;
+      }
+    }
+  } else if (
+    rangeStart < 0 ||
+    rangeEnd < 0 ||
+    typeof rangeStart === "string" ||
+    typeof rangeEnd === "string" ||
+    typeof rangeStart != "number" ||
+    typeof rangeEnd != "number"
+  ) {
+    return "ERROR";
+  } else {
+    console.log("error");
+  }
+  return sum;
 };
 
 // Do not edit below this line
