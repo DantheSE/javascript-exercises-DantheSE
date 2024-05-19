ce1d757c708138da12865d35acc1a59e90436336
DantheSE <dan.doyin@gmail.com>
2024-05-19 23:58:59 +0100

    finished challenge 7, all tests passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,20 @@
-const convertToCelsius = function() {
+let celsiusValue;
+let fahrenheitValue;
+
+const convertToCelsius = function (fahrenheitValue) {
+  let convertedCelsius = ((fahrenheitValue - 32) * 5) / 9;
+  let roundedCelsius = Math.round(convertedCelsius * 10) / 10;
+  return roundedCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsiusValue) {
+  let convertedFahrenheit = (celsiusValue * 9) / 5 + 32;
+  let roundedFahrenheit = Math.round(convertedFahrenheit * 10) / 10;
+  return roundedFahrenheit;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
