ff4f5dadfb9764ff350b0e70e9ad9742e31675f8
DantheSE <dan.doyin@gmail.com>
2024-05-19 02:55:13 +0100

    completed the repeatString module, code passed all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+let inputString;
+let numberOfReps;
+let stringStorage = "";
+let errorMessage;
+const repeatString = function (inputString, numberOfReps) {
+  stringStorage = "";
+  if (numberOfReps < 0) {
+    return "ERROR";
+  }
+  for (let i = 1; i <= numberOfReps; i++) {
+    stringStorage += inputString;
+  }
+  return stringStorage;
 };
 
 // Do not edit below this line
